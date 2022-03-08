c2929a8a44c04dd59f550da80ae9190330c6e44c
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2022-03-08 15:50:31 +0100

    Add cat command

---
+++ commands/dog.js
@@ -0,0 +1,32 @@
+const { Permissions, MessageEmbed } = require("discord.js");
+const axios = require("axios")
+
+module.exports = {
+    description: "Get a random dog",
+    usage: "",
+    permission: null,
+    guild: false,
+    run: async (bot, message, args) => {
+        // Create a random title with a random ending
+        const titles = ["Dog payload", "Woof woof woof", "Incoming puppy", "Detected cuteness"];
+        const emoticons = [" :)", " :D", " <3", "!"];
+        const randTitle = titles[Math.floor(Math.random() * titles.length)] + emoticons[Math.floor(Math.random() * emoticons.length)];
+        // Fetch the dog images
+        dog = await axios.get("https://api.thedogapi.com/v1/images/search");
+        dogImage = dog.data[0].url;
+        // Create a new embed
+        const embed = new MessageEmbed()
+        .setTitle(randTitle)
+        .setAuthor({
+            name: message.author.tag,
+            url: dogImage,
+            iconURL: message.author.displayAvatarURL()
+        })
+        .setImage(dogImage)
+        .setURL(dogImage)
+        .setColor(0xff6961)
+        .setTimestamp();
+	// Reply to the author with the embed
+        message.reply({embeds: [embed]})
+    }
+}
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -66,5 +66,6 @@ async function catEmotes(bot, message) {
 
 async function mentionReponse(bot, message) {
     if (message.author.bot) return;
-    if (message.mentions.has(bot.client.user.id)) message.reply(`My prefix is \`${bot.config.prefix}\` :)`)
+    let replyString = "My prefix is " + (Math.floor(Math.random() * 100) === 42 ? ">:(" : ":)");
+    if (message.mentions.has(bot.client.user.id)) message.reply(replyString);
 }
