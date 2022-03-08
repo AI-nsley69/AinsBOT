3f084cc26c1c0a064973524b5887d1edbbbfde1b
AI-nsley69 <not@ssis-l0616.ad.ssis.nu>
2022-03-08 09:50:20 +0100

    Admin command: reply

---
+++ commands/admin/reply.js
@@ -0,0 +1,15 @@
+const { AwaitMessageOptions } = require("discord.js");
+
+module.exports = {
+    description: "Reply with to a message with given string",
+    usage: "[msg id to respond to] [string to repeat]",
+    run: async (bot, message, args) => {
+        message.delete().catch();
+        let targetMsgId = args.shift();
+        // Get the message
+        let msg = await message.channel.messages.fetch(targetMsgId);
+        // Check if we got the message, if so reply to it, otherwise let the author know
+        if (msg) msg.reply(args.join(" "));
+        else message.channel.send("Could not find message!").then(m => setTimeout(() => m.delete(), 3000))
+    }
+}
