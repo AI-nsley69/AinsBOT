40852dd6aa83c68e82d2377b505ea927c517bb86
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2022-06-25 02:21:11 +0200

    Cuddle & kiss command

---
+++ commands/cuddle.js
@@ -0,0 +1,27 @@
+const { Permissions, MessageEmbed } = require("discord.js");
+const axios = require("axios");
+
+module.exports = {
+    description: "Cuddle a user!",
+    usage: "[user]",
+    permission: null,
+    guild: false,
+    run: async (bot, message, args) => {
+        // Verify that we have a user to hug and if it is valid
+        const member = message.mentions.members.first();
+        if (!member) return bot.utils.softErr(bot, message, "Awh.. got no one to cuddle? ):");
+        if (member.user.id === message.author.id) return bot.utils.softErr(bot, message, "You can't cuddle yourself silly!");
+        // Temporary loading message
+        const msg = await bot.utils.cmdLoadingMsg(bot, message);
+        // Fetch the media
+	const cuddle = await axios.get("https://api.otakugifs.xyz/gif?reaction=cuddle&format=gif");
+	// Embed to send
+        const embed = new MessageEmbed()
+        .setTitle(`${member.user.tag} cuddled ${message.author.tag}!`)
+        .setColor(bot.consts.Colors.INFO)
+        .setImage(cuddle.data.url)
+        .setTimestamp()
+        // Edit message to include the new embed
+        msg.edit({ embeds: [embed] });
+    }
+}
---
+++ commands/kiss.js
@@ -0,0 +1,27 @@
+const { Permissions, MessageEmbed } = require("discord.js");
+const axios = require("axios");
+
+module.exports = {
+    description: "Kiss a user!",
+    usage: "[user]",
+    permission: null,
+    guild: false,
+    run: async (bot, message, args) => {
+        // Verify that we have a user to hug and if it is valid
+        const member = message.mentions.members.first();
+        if (!member) return bot.utils.softErr(bot, message, "Awh.. got no one to kiss? ):");
+        if (member.user.id === message.author.id) return bot.utils.softErr(bot, message, "You can't kiss yourself silly!");
+        // Temporary loading message
+        const msg = await bot.utils.cmdLoadingMsg(bot, message);
+        // Fetch the media
+	const kiss = await axios.get("https://api.otakugifs.xyz/gif?reaction=kiss&format=gif");
+	// Embed to send
+        const embed = new MessageEmbed()
+        .setTitle(`${member.user.tag} kissed ${message.author.tag}!`)
+        .setColor(bot.consts.Colors.INFO)
+        .setImage(kiss.data.url)
+        .setTimestamp()
+        // Edit message to include the new embed
+        msg.edit({ embeds: [embed] });
+    }
+}
