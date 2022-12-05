c6dfd43f1547f9e1d219c91b9a51d6f018880639
20bear <20bear@stockholmscience.se>
2022-12-05 13:35:45 +0100

    Make the ping command fancier, per openai chat's suggestion

--- commands/ping.js
+++ commands/ping.js
@@ -1,27 +1,40 @@
 const { Permissions, MessageEmbed } = require("discord.js");
 
 module.exports = {
-    description: "Ping pong!",
-    usage: "",
-    permission: null,
-    botPermissions: [],    
-    guild: false,
-    cooldown: 0,
-    run: async (bot, message, loadingMsg, args) => {
-        // Get ping by subtracting the created timestamp from the current timestamp
-        const ping = Date.now() - message.createdTimestamp;
-        // Get the color by dividing ping by 1000 and multiplying it by 255 and then convert to hex
-        const pingDivider = 250; // Modify this as needed, currently ping is usually 10-100ms in my situation
-	const color = (ping / pingDivider * 255) < 256 ? 255 - (Math.floor(ping / pingDivider * 255)) : 0;
-	// Create the message embed
-        const embed = new MessageEmbed()
-        .setTitle("Pong!")
-        .setDescription(`${ping}ms`)
-        .setColor(color)
-        .setFooter({
-            text: "Note: the ping is between Discord API and the bot!"
-        });
-
-        loadingMsg.edit({ embeds: [embed] });
+  description: "Ping pong!",
+  usage: "",
+  permission: null,
+  botPermissions: [],
+  guild: false,
+  cooldown: 0,
+  run: async (bot, message, loadingMsg, args) => {
+    // Get ping by subtracting the created timestamp from the current timestamp
+    const ping = Date.now() - message.createdTimestamp;
+    // Set up the title and color depending on the ping value
+    let title = "Pong!";
+    let color = 0x000000;
+    if (ping < 100) {
+      title = "🟢 Super fast!";
+      color = 0x00ff00;
+    } else if (ping < 250) {
+      title = "🟡 Pretty fast!";
+      color = 0xffff00;
+    } else if (ping < 500) {
+      title = "🟠 Kind of slow..";
+      color = 0xff9f00;
+    } else {
+      title = "🔴 Uh oh, slow!";
+      color = 0xff0000;
     }
-}
+    // Create the message embed
+    const embed = new MessageEmbed()
+      .setTitle(title)
+      .setDescription(`Pong! 🏓\nCurrent latency: **${ping}ms**`)
+      .setColor(color)
+      .setFooter({
+        text: "Note: the ping is between Discord API and the bot!",
+      });
+
+    loadingMsg.edit({ embeds: [embed] });
+  },
+};
