ca71c1155dd12746d04f06844817c34a0399c816
20bear <20bear@stockholmscience.se>
2022-12-05 11:25:38 +0100

    OpenAI code optimization suggestions

--- index.js
+++ index.js
@@ -5,85 +5,99 @@ const { ImgurClient } = require("imgur");
 // Create intents for discord bot so library is satisfied"
 const intents = new Intents();
 intents.add(
-    Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MEMBERS
-)
+  Intents.FLAGS.GUILDS,
+  Intents.FLAGS.DIRECT_MESSAGES,
+  Intents.FLAGS.GUILD_MESSAGES,
+  Intents.FLAGS.GUILD_MEMBERS
+);
 // Create a new client object, load the dotenv config and login as the bot
 const client = new Client({
-    intents: intents,
-    partials: ["CHANNEL"]
+  intents: intents,
+  partials: ["CHANNEL"],
 });
 dotenv.config();
 client.login(process.env.token);
 
 const bot = {
-    client: client,
-    fs: require("fs"),
-    logger: require("./modules/logger.js"),
-    utils: require("./modules/utils.js"),
-    consts: require("./modules/constants.js"),
-    sequelize: new Sequelize("database", "user", "password", {
-        host: "localhost",
-        dialect: "sqlite",
-        logging: false,
-        storage: "database.sqlite"
-    }),
-    db: {},
-    imgur: new ImgurClient({ clientId: process.env.imgurId }),
-    passthroughs: [],
-    bridges: []
-}
+  client: client,
+  fs: require("fs"),
+  logger: require("./modules/logger.js"),
+  utils: require("./modules/utils.js"),
+  consts: require("./modules/constants.js"),
+  sequelize: new Sequelize("database", "user", "password", {
+    host: "localhost",
+    dialect: "sqlite",
+    logging: false,
+    storage: "database.sqlite",
+  }),
+  db: {},
+  imgur: new ImgurClient({ clientId: process.env.imgurId }),
+  passthroughs: [],
+  bridges: [],
+};
 // Database for toggling features
 bot.db.features = bot.sequelize.define("features", {
-    guildId: {
-        type: Sequelize.STRING,
-        unique: true,
-    },
-    tiktokPreview: Sequelize.BOOLEAN,
-    messagePreview: Sequelize.BOOLEAN,
-    redditPreview: Sequelize.BOOLEAN
+  guildId: {
+    type: Sequelize.STRING,
+    unique: true,
+  },
+  tiktokPreview: Sequelize.BOOLEAN,
+  messagePreview: Sequelize.BOOLEAN,
+  redditPreview: Sequelize.BOOLEAN,
 });
 // Database for toggling commands
 bot.db.commands = bot.sequelize.define("commands", {
-    guildId: {
-        type: Sequelize.STRING,
-        unique: true,
-    },
-    disabled: Sequelize.STRING    
+  guildId: {
+    type: Sequelize.STRING,
+    unique: true,
+  },
+  disabled: Sequelize.STRING,
 });
 // Database for bot channel
 bot.db.botChannels = bot.sequelize.define("bot_channels", {
-    guildId: {
-        type: Sequelize.STRING,
-        unique: true
-    },
-    bot_channel: Sequelize.STRING
+  guildId: {
+    type: Sequelize.STRING,
+    unique: true,
+  },
+  bot_channel: Sequelize.STRING,
 });
 
 bot.config = JSON.parse(bot.fs.readFileSync("./config.json"));
 
 // Read the directory containing commands and then add them to a map
-bot.commandFiles = bot.fs.readdirSync("./commands/").filter(file => file.endsWith(".js"));
-bot.commands = new Map();
-for (let i = 0; i < bot.commandFiles.length; i++) {
-    bot.commands.set(bot.commandFiles[i].replace(".js", ""), require(`./commands/${bot.commandFiles[i]}`));
-}
+bot.commandFiles = bot.fs
+  .readdirSync("./commands/")
+  .filter((file) => file.endsWith(".js"));
+bot.commands = new Map(
+  bot.commandFiles.map((file) => [
+    file.replace(".js", ""),
+    require(`./commands/${file}`),
+  ])
+);
 // Repeat for admin commands
-bot.adminCommandFiles = bot.fs.readdirSync("./commands/admin/").filter(file => file.endsWith(".js"));
-bot.adminCommands = new Map();
-for (let i = 0; i < bot.adminCommandFiles.length; i++) {
-    bot.adminCommands.set(bot.adminCommandFiles[i].replace(".js", ""), require(`./commands/admin/${bot.adminCommandFiles[i]}`));
-}
+bot.adminCommandFiles = bot.fs
+  .readdirSync("./commands/admin/")
+  .filter((file) => file.endsWith(".js"));
+bot.adminCommands = new Map(
+  bot.adminCommandFiles.map((file) => [
+    file.replace(".js", ""),
+    require(`./commands/admin/${file}`),
+  ])
+);
 // Repeat, again, for events
-bot.eventFiles = bot.fs.readdirSync("./events/").filter(file => file.endsWith(".js"));
-bot.events = new Map();
-for (let i = 0; i < bot.eventFiles.length; i++) {
-    bot.events.set(bot.eventFiles[i].replace(".js", ""), require(`./events/${bot.eventFiles[i]}`));
-}
+bot.eventFiles = bot.fs
+  .readdirSync("./events/")
+  .filter((file) => file.endsWith(".js"));
+bot.events = new Map(
+  bot.eventFiles.map((file) => [
+    file.replace(".js", ""),
+    require(`./events/${file}`),
+  ])
+);
 
 bot.events.forEach((v, eventName, events) => {
-    const event = bot.events.get(eventName);
-    bot.client.on(eventName, async (...args) => {
-        event.run(bot, ...args).catch(err => console.log(err));
-    })
-})
+  const event = bot.events.get(eventName);
+  bot.client.on(eventName, async (...args) => {
+    event.run(bot, ...args).catch((err) => console.log(err));
+  });
+});
