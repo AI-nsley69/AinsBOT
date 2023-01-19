74654985a1b793e00a87cdfdd0ece59570922403
20bear <20bear@stockholmscience.se>
2023-01-19 22:03:47 +0100

    soft error if no invite link is configured

--- commands/information/discord.js
+++ commands/information/discord.js
@@ -8,6 +8,7 @@ module.exports = {
 	guild: false,
 	cooldown: 0,
 	run: async (bot, message, loadingMsg) => {
+		if (!process.env.inviteLink) return bot.utils.softErr(bot, message, 'No invite link is configured!', loadingMsg);
 		const embed = new MessageEmbed()
 			.setTitle(`${bot.client.user.username}'s server!`)
 			.setURL(process.env.inviteLink)
