79ae3d6171fa741cf723f8b55cd071aca748ace9
20bear <20bear@stockholmscience.se>
2023-01-20 12:48:02 +0100

    Use chalk in logger for colors

--- modules/logger.js
+++ modules/logger.js
@@ -1,22 +1,23 @@
+const chalk = require('chalk');
 const file = './logs/latest.log';
 
 module.exports = {
 	err: async (bot, toLog) => {
-		log(bot, toLog, 'ERR');
+		log(bot, toLog, chalk.redBright('ERR'));
 	},
 	verbose: async (bot, toLog) => {
 		if (bot.config.logLevel !== 'verbose') return;
-		log(bot, toLog, 'VERBOSE');
+		log(bot, toLog, chalk.greenBright('VERBOSE'));
 	},
 	warn: async (bot, toLog) => {
 		if (bot.config.logLevel !== 'warn' || bot.config.logLevel !== 'verbose') return;
-		log(bot, toLog, 'WARN');
+		log(bot, toLog, chalk.rgb(255, 139, 40)('WARN'));
 	},
 };
 
 function log(bot, str, level) {
 	const currentTime = new Date();
-	str = `(${currentTime.toTimeString().split(' ')[0]}) [LOG/${level}] ${str}`;
+	str = `(${chalk.magentaBright(currentTime.toTimeString().split(' ')[0])}) [${chalk.yellow('LOG')}/${level}] ${str}`;
 	console.log(str);
 	bot.fs.appendFile(file, `${str}\n`, (err) => {
 		if (err) throw err;
