dc35789fe4a5f63b0bd0d5b2ddd01bcf295c2396
20bear <20bear@stockholmscience.se>
2023-01-20 22:19:58 +0100

    Catch all errors from functions in messageCreate

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -8,21 +8,21 @@ async function run(bot, message) {
 	// Check if the author is a bot
 	if (message.author.bot) {return;}
 	// Command handlers
-	commandHandler(bot, message);
-	adminCommandHandler(bot, message);
+	commandHandler(bot, message).catch(err => bot.logger.err(err.toString()));
+	adminCommandHandler(bot, message).catch(err => bot.logger.err(err.toString()));
 	// Dm relay
-	dmRelay(bot, message);
+	dmRelay(bot, message).catch(err => bot.logger.err(err.toString()));
 	// Misc
 	// Hardcoded to work for my personal guild with my cat emote guild
-	catEmotes(bot, message);
+	catEmotes(bot, message).catch(err => bot.logger.err(err.toString()));
 	// mentionReponse(bot, message);
 	// Features
-	getTiktok(bot, message);
-	previewMessage(bot, message);
-	previewReddit(bot, message);
+	getTiktok(bot, message).catch(err => bot.logger.err(err.toString()));
+	previewMessage(bot, message).catch(err => bot.logger.err(err.toString()));
+	previewReddit(bot, message).catch(err => bot.logger.err(err.toString()));
 	// Channel bridge & passthrough
-	channelPassthrough(bot, message);
-	channelBridging(bot, message);
+	channelPassthrough(bot, message).catch(err => bot.logger.err(err.toString()));
+	channelBridging(bot, message).catch(err => bot.logger.err(err.toString()));
 }
 
 export default { run };
