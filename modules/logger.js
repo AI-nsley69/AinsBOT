3a72ae68864aebf17007923c2550c59f08f79100
20bear <20bear@stockholmscience.se>
2023-01-21 01:09:33 +0100

    Split logger into console and saveToFile function

--- modules/logger.js
+++ modules/logger.js
@@ -1,25 +1,33 @@
 import chalk from 'chalk';
 import fs from 'fs';
 const file = './logs/latest.log';
+let currentTime;
 
 async function err(bot, toLog) {
-	log(bot, toLog, chalk.redBright('ERR'));
+	console(toLog, chalk.redBright('ERR'));
+	saveToFile(toLog, 'ERR');
 }
 async function verbose(bot, toLog) {
 	if (bot.config.logLevel !== 'verbose') {return;}
-	log(bot, toLog, chalk.greenBright('VERBOSE'));
+	console(toLog, chalk.greenBright('VERBOSE'));
+	saveToFile(toLog, 'VERBOSE');
 }
 async function warn(bot, toLog) {
 	if (bot.config.logLevel !== 'warn' || bot.config.logLevel !== 'verbose') {return;}
-	log(bot, toLog, chalk.rgb(255, 139, 40)('WARN'));
+	console(toLog, chalk.rgb(255, 139, 40)('WARN'));
+	saveToFile(toLog, 'WARN');
 }
 
 export default { err, verbose, warn };
 
-function log(bot, str, level) {
-	const currentTime = new Date();
+function console(str, level) {
+	currentTime = new Date();
 	str = `(${chalk.magentaBright(currentTime.toTimeString().split(' ')[0])}) [${chalk.yellow('LOG')}/${level}] ${str}`;
 	console.log(str);
+}
+
+function saveToFile(str, level) {
+	str = `(${currentTime.toTimeString().split(' ')[0]}) [${'LOG'}/${level}] ${str}`;
 	fs.appendFile(file, `${str}\n`, (err) => {
 		if (err) throw err;
 	});
