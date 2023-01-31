708017a0638bd6037f4892b26c6bb8d2dbe93669
20bear <20bear@stockholmscience.se>
2023-01-31 10:59:24 +0100

    mapper constant on catch for messageCreate

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -5,22 +5,23 @@ import { dmRelay, channelBridging, channelPassthrough } from './messageCreate/br
 
 async function run(bot, message) {
 	if (message.author.bot) {return;}
+	const logError = err => bot.logger.err(err);
 	// Command handlers
-	commandHandler(bot, message);
-	adminCommandHandler(bot, message).catch(err => bot.logger.err(err));
+	commandHandler(bot, message).catch(logError);
+	adminCommandHandler(bot, message).catch(logError);
 	// Dm relay
-	dmRelay(bot, message).catch(err => bot.logger.err(err));
+	dmRelay(bot, message).catch(logError);
 	// Misc
 	// Hardcoded to work for my personal guild with my cat emote guild
-	catEmotes(bot, message).catch(err => bot.logger.err(err));
+	catEmotes(bot, message).catch(logError);
 	// mentionReponse(bot, message);
 	// Features
-	// getTiktok(bot, message).catch(err => bot.logger.err(err));
-	previewMessage(bot, message).catch(err => bot.logger.err(err));
-	previewReddit(bot, message).catch(err => bot.logger.err(err));
+	// getTiktok(bot, message).catch(logError);
+	previewMessage(bot, message).catch(logError);
+	previewReddit(bot, message).catch(logError);
 	// Channel bridge & passthrough
-	channelPassthrough(bot, message).catch(err => bot.logger.err(err));
-	channelBridging(bot, message).catch(err => bot.logger.err(err));
+	channelPassthrough(bot, message).catch(logError);
+	channelBridging(bot, message).catch(logError);
 }
 
 export default { run };
